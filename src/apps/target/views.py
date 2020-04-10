9fbd59ec8973be2e573f29f87e7b097e1af9c107
TMS 13 <gotestweb@gmail.com>
2020-04-10 21:49:15 +0300

    1

--- src/apps/target/views.py
+++ src/apps/target/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 from django.views.decorators.cache import cache_control
 
 from project.utils import consts
@@ -8,4 +9,10 @@ from project.utils import consts
 
 @cache_control(max_age=consts.AGE_1DAY)
 def view_index(request: HttpRequest) -> HttpResponse:
+    if request.method != "GET":
+        return HttpResponse(" ", status=405)
     return render(request, "target/index.html")
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, "target/index.html")
